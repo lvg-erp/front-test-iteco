1691e9aab29c4edce3bea4fd104e42fe47c04cb7
lvg-erp <profmail@yandex.ru>
2022-11-01 17:14:12 +0300

    Merged branch_name

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   // <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter> 
   // </React.StrictMode>
 );
